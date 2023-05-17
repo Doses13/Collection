9596f61486ebf7efea5c43b65c02ddb2d6b2470e
Joseph Byers <shyguy383@outlook.com>
2023-05-16 17:01:57 -0700

    new class files

---
+++ Collection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace collectionTest1
+{
+    public class Collection
+    {
+        public string name;
+        public List<Item> items = new List<Item>();
+        public List<string> attributes = new List<string>();
+    }
+}
---
+++ Item.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace collectionTest1
+{
+    public class Item
+    {
+        public string name;
+        public List<string> attributes = new List<string>();
+    }
+}
