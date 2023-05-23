8f2d9d37836a2af01f28305941265d6bac4c9c9c
gabrielaabafia <gabrielaabafia@gmail.com>
2023-05-23 15:52:15 -0700

    SingleView Edit and Attributes

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -225,10 +225,27 @@ namespace collectionTest1
         private void AddedItemImage_PointerPressed(object sender, PointerRoutedEventArgs e)
         {
             changeScreen(screens.Single);
-            Item item = collectionList[activeCollection].items.Find(x => x.image == (sender as Image));
-            singleViewName.Text = item.name;
-            singleViewDescription.Text = item.description;
+            curItem = collectionList[activeCollection].items.Find(x => x.image == (sender as Image));
+            singleViewName.Text = curItem.name;
+            singleViewDescription.Text = curItem.description;
             singleItemImage.Source = (sender as Image).Source;
+
+            for(int i = 0; i < collectionList[activeCollection].attributes.Count; i++)
+            {
+                TextBox attributeTextBox = new TextBox();
+                attributeTextBox.Header = collectionList[activeCollection].attributes[i];
+                attributeTextBox.PlaceholderText = curItem.attributes[i];
+
+                //attributeTextBox.Height = Auto;
+                //attributeTextBox.Width = Auto;
+
+                attributeTextBox.Margin = new Thickness(20);
+                attributeTextBox.HorizontalAlignment = HorizontalAlignment.Stretch;
+                attributeTextBox.IsEnabled = false;
+
+                attributePanel.Children.Add(attributeTextBox);
+            }
+
         }
 
 
@@ -461,6 +478,7 @@ namespace collectionTest1
         public void NewCollectionConfirm(object sender, RoutedEventArgs e)
         {
             if(!string.IsNullOrEmpty(colName.Text))
+            { 
             
                 collectionList.Last().name = colName.Text;
                 colName.Text = "";
@@ -520,9 +538,18 @@ namespace collectionTest1
             singleViewToggleEditing();
         }
 
+        Item curItem = null;
+
         public void singleViewSaveClick(object sender, RoutedEventArgs e)
         {
             singleViewToggleEditing();
+            curItem.name = singleViewName.Text;
+            curItem.description = singleViewDescription.Text;
+
+            foreach (string attribute in curItem.attributes)
+            {
+                //Need to be able to update attributes
+            }
 
         }
 
